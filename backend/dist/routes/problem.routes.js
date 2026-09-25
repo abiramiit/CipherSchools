796a00cb91d67d6dcafdacaa912c7b9345d7d2ea
abiramiit <abirami.s2024it@sece.ac.in>
2026-09-25 08:58:40 +0530

    fix: resolve production problem seeding and dependencies

--- backend/dist/routes/problem.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.problemRoutes = void 0;
-const express_1 = require("express");
-const problem_controller_1 = require("../controllers/problem.controller");
-exports.problemRoutes = (0, express_1.Router)();
-const controller = new problem_controller_1.ProblemController();
-exports.problemRoutes.get('/', controller.getAll);
-exports.problemRoutes.get('/:id', controller.getById);
